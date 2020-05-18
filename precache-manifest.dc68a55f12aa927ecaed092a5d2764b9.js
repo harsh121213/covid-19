b33cfd8d21a4cfc0586e6f5fbae1aa7efcea8d1f
harsh <harshtiwary45@gmail.com>
2020-05-19 02:32:28 +0530

    Updates

---
+++ precache-manifest.dc68a55f12aa927ecaed092a5d2764b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97936c9fa6db06983fdf700bab30209",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "4b6a46c92f486a8c7b9c",
+    "url": "/covid/static/css/main.9cb29be3.chunk.css"
+  },
+  {
+    "revision": "1b69ba4d1091029f7cce",
+    "url": "/covid/static/js/2.43b3be15.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.43b3be15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6a46c92f486a8c7b9c",
+    "url": "/covid/static/js/main.34fd35a7.chunk.js"
+  },
+  {
+    "revision": "1c947891d37bc70c2997",
+    "url": "/covid/static/js/runtime-main.8cffd104.js"
+  },
+  {
+    "revision": "08708a2fc6a99202028d4671a9f7755e",
+    "url": "/covid/static/media/covid.08708a2f.png"
+  }
+]);
